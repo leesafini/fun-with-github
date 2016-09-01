fd213f30303a5efc3d8a0d17b27570a9c1c7b213
Kathy <kchen2013@gmail.com>
2016-09-01 16:10:41 -0400

    underflow case added to DumbMath

--- DumbMath.java
+++ DumbMath.java
@@ -9,14 +9,13 @@ public class DumbMath {
 
 	public static int add(int a, int b) {
 		int sum = a + b;
-		
+
 		// check for overflow
 		if (a > 0 && b > 0 && sum < 0)
 			throw new ArithmeticException("Overflow when adding " + a + " and " + b);
-
 		// check for underflow
 		if (a < 0 && b < 0 && sum > 0)
-			throw new ArithmeticException("Underflow when adding " + a + " and " + b);			
+			throw new ArithmeticException("Underflow when adding " + a + " and " + b);
 		return sum;
 	}
 
@@ -27,7 +26,9 @@ public class DumbMath {
     // check for overflow
     if (a > 0 && b > 0 && sum < 0)
       throw new ArithmeticException("Overflow when adding " + a + " and " + b);
-
+    // check for underflow
+    if (a < 0 && b < 0 && sum > 0)
+        throw new ArithmeticException("Underflow when adding " + a + " and " + b);
     return sum;
   }
 
@@ -36,15 +37,15 @@ public class DumbMath {
 		int product = a * b;
 		return product;
 	}
-	
+
 
 	public static double addFraction(int n1, int d1, int n2, int d2) {
 		if (d1 == 0 || d2 == 0)
 			throw new IllegalArgumentException("Denominator is zero!");
-		
+
 		double f1 = ((double)n1)/d1;
 		double f2 = ((double)n2)/d2;
 		return f1 + f2;
-		
+
 	}
 }
