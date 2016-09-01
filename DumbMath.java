67948b678f9f712f1adf46b527e80f6f58220974
karenher <karenher@seas.upenn.edu>
2016-09-01 15:35:57 -0400

    removed unnecessary code in DumbMath.addFraction

--- DumbMath.java
+++ DumbMath.java
@@ -35,11 +35,6 @@ public class DumbMath {
 	public static double addFraction(int n1, int d1, int n2, int d2) {
 		if (d1 == 0 || d2 == 0)
 			throw new IllegalArgumentException("Denominator is zero!");
-
-		if (n1 == 0)
-			return ((double)n2)/d2;
-		if (n2 == 0)
-			return ((double)n1)/d1;
 		
 		double f1 = ((double)n1)/d1;
 		double f2 = ((double)n2)/d2;
